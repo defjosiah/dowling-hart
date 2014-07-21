9fc1222caf4291217da999365a636118adc7ee64
Josiah Grace <josiahbgrace@gmail.com>
2014-07-20 23:34:21 -0600

    the individual dots now adjust based on the date and a buffer, so sick

--- application.py
+++ application.py
@@ -11,15 +11,28 @@ def index():
 
 @app.route('/background/')
 def background():
-    name_year = return_file_years("./text/background")
+    name_year = calculate_timeline_placement( 
+                    return_file_years("./text/background"), 10)
     return render_template('background.html', items=name_year)
 
-def calculate_timeline_placement(buffer):
+def calculate_timeline_placement(name_year, buf):
     """
     Calculate the individual placement of each of the timeline circles using
-    the input name_year information. 
-    """
+    the input name_year information. The buffer is the percent that needs to 
+    be left open on either side for the timeline. Sorted by year. 
+    (doc_name, distance_percent, info_tuple) 
+    """     
+    #unpack the values in name_year
+    year_span, docs = name_year
+    min_year = year_span[1]
+    background_temp = []
 
+    for doc, info in docs.items():
+        if info["date"] != "none":
+            distance = (int(info["date"])*1.0 - min_year)/147 * (100 - buf*2) + buf
+            background_temp.append( (doc, distance, info) )
+
+    return sorted(background_temp, key=lambda x: x[1])
 
 
 if __name__ == '__main__':
